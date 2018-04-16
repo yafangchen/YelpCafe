35f7ace1e2a014c7aa2f51adf174bb1fc672e8d7
yafangchen <yafangchen89@gmail.com>
2018-04-16 01:39:40 -0700

    Add cafe view

--- src/app/components/cafe/cafe-view/cafe-view.component.ts
+++ src/app/components/cafe/cafe-view/cafe-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router} from '@angular/router';
+import { CafeService } from '../../../services/cafe.service.client';
 
 @Component({
   selector: 'app-cafe-view',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cafe-view.component.css']
 })
 export class CafeViewComponent implements OnInit {
-
-  constructor() { }
+  cafeProfileId: String;
+  name: String;
+  address: String;
+  openHour: String
+  menus: Array;
+  reviews: Array;
+  constructor(private activatedRoute: ActivatedRoute, private _cafeServie: CafeService, private router: Router) {}
 
   ngOnInit() {
+      this.activatedRoute.params.subscribe(params => {
+          this.cafeProfileId = params['cafeProfileId'];
+      });
+      this._cafeServie.findCafeById(this.cafeProfileId)
+          .subscribe(
+              (data: any) => {
+                  console.log('website by id: ', data);
+                  this.name = data.name;
+                  this.address = data.address;
+                  this.openHour = data.open_hour;
+                  this.menus = data.menus;
+                  this.reviews = data.reviews;
+              }
+          );
   }
-
 }
